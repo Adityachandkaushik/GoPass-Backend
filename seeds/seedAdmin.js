bf001ff913aac1a95b6349a07261b68ad0d75443
Aditya kaushik <165358967+Adityachandkaushik@users.noreply.github.com>
2026-09-21 12:09:28 +0530

    Fix formatting in seedAdmin.js

--- seeds/seedAdmin.js
+++ seeds/seedAdmin.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const User = require("../models/user");
-const connectDB = require('../config/db');
+const connectDB = require('../config/db');  
 
 const seedAdmin = async () => {
   await connectDB();
